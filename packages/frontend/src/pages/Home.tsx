4ae48cccd95b6b82ef42791553c0b2b6dcdf798d
gideonparanoid <gideon.jones@dazn.com>
2020-05-26 22:04:28 +0100

    swap chart lib

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,36 +1,33 @@
 import * as React from 'react';
 import useAxios from 'axios-hooks';
-import { LineChart, XAxis, YAxis, Line, CartesianGrid } from 'recharts';
+import { Line } from 'react-chartjs-2';
 
 const url = 'http://192.168.1.1:8081';
 
 export const Home = () => {
   const [{ data }] = useAxios(`${url}/day`);
 
-  const [more] = useAxios(`${url}/day/2020-05-26/minute`);
+  const [today] = useAxios(`${url}/day/2020-05-26/minute`);
 
-  let todayChart = (
-    <b>loading</b>
-  );
+  let todayChart = <b>loading</b>;
 
-  if (more.data) {
-    console.log('more',more);
+  if (today.data) {
+    const dat = {
+      labels: today.data.map(({ date }) => date),
+      datasets: [
+        {
+          label: 'power',
+          data: today.data.map(({ pv }) => pv.power),
+        },
+      ],
+    };
 
-    todayChart = (
-      <LineChart width={1000} height={600} data={more.data}>
-        <XAxis dataKey="name"/>
-        <YAxis/>
-        <CartesianGrid stroke="#eee" strokeDasharray="100 100"/>
-        <Line type="monotone" dataKey="pv.power" stroke="#8884d8" />
-        </LineChart>
-    )
+    todayChart = <Line data={dat} />;
   }
-
 
   if (data) {
     console.log(data);
   }
-
 
   return (
     <div>
@@ -38,14 +35,13 @@ export const Home = () => {
 
       {todayChart}
       <ul>
-        {data && data.map(({ date, total }) => (
-          <li>{date} - {total}</li>
-        ))}
+        {data &&
+          data.map(({ date, total }) => (
+            <li key={date}>
+              {date} - {total}
+            </li>
+          ))}
       </ul>
-
-
-
-
-  </div>
+    </div>
   );
 };
