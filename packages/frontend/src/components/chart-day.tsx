6e7cd5e66c8d263eb4a87d98532657907307aceb
gideonparanoid <gideon.jones@dazn.com>
2020-05-29 22:19:51 +0100

    better style for chart

--- packages/frontend/src/components/chart-day.tsx
+++ packages/frontend/src/components/chart-day.tsx
@@ -3,7 +3,7 @@ import { Line } from 'react-chartjs-2';
 
 import { MinuteStats } from 'solax-common/types';
 
-import { getTime, movingAverage } from '@/utils';
+import { movingAverage } from '@/utils';
 
 export interface IChartDay {
   data: MinuteStats[];
@@ -13,18 +13,36 @@ export const ChartDay: FunctionComponent<IChartDay> = ({ data }) => {
   const powerValues = data.map(({ pv }: MinuteStats) => pv.power);
 
   const chart = {
-    labels: data.map(({ date }: MinuteStats) => getTime(new Date(date))),
+    labels: data.map(({ date }: MinuteStats) => new Date(date)),
     datasets: [
       {
         label: 'Power (watts)',
         data: powerValues,
+        fill: false,
+        pointRadius: 0,
       },
       {
-        label: 'Hourly moving average power (watts) ',
+        label: 'Hour moving average power (watts) ',
         data: movingAverage(powerValues, 60),
+        backgroundColor: 'rgba(0, 0, 128, 0.5)',
+        pointRadius: 0,
       },
     ],
   };
 
-  return <Line data={chart} />;
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'minute',
+            stepSize: 5,
+          },
+        },
+      ],
+    },
+  };
+
+  return <Line data={chart} options={options} />;
 };
